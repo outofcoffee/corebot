fc47b811394a251d8305f1226d70ddc5605debdb
Pete Cornish <outofcoffee@gmail.com>
2017-05-22 00:58:02 +0100

    Build multiple Docker images.

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,36 +1,56 @@
 #!/usr/bin/env bash
 set -e
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKERFILE_PATH="${SCRIPT_DIR}/../bots/slack-"
 IMAGE_BASE_NAME="outofcoffee/corebot"
 IMAGE_TAG="${1-dev}"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+IMAGES=(
+    "deploy"
+    "items"
+)
 
 function buildImage()
 {
-    IMAGE_NAME="${IMAGE_BASE_NAME}:${IMAGE_TAG}"
-    echo -e "\nBuilding Docker image: ${IMAGE_NAME}"
+    IMAGE_SUFFIX="$2"
+    IMAGE_NAME="${IMAGE_BASE_NAME}${IMAGE_SUFFIX}:${IMAGE_TAG}"
 
+    echo -e "\nBuilding Docker image: ${IMAGE_NAME}"
     cd $1
     docker build --tag ${IMAGE_NAME} .
 }
 
-
 function pushImage()
 {
+    IMAGE_SUFFIX="$1"
+    IMAGE_NAME="${IMAGE_BASE_NAME}${IMAGE_SUFFIX}:${IMAGE_TAG}"
+
     echo -e "\nLogging in to Docker registry..."
     docker login --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}" --email deprecated@example.com
 
-    IMAGE_NAME="${IMAGE_BASE_NAME}:${IMAGE_TAG}"
     echo -e "\nPushing Docker image: ${IMAGE_NAME}"
-
     docker push ${IMAGE_NAME}
 }
 
-echo -e "\nBuilding image from local source"
-buildImage "${SCRIPT_DIR}/../"
+function buildPushImage()
+{
+    IMAGE_DIR="$1"
+    echo -e "\nBuilding '${IMAGE_DIR}' image"
 
-if [[ "dev" == "${IMAGE_TAG}" ]]; then
-    echo -e "\nSkipped pushing dev image"
-else
-    pushImage
-fi
+    if [[ "deploy" == "${IMAGE_DIR}" ]]; then
+        IMAGE_SUFFIX=""
+    else
+        IMAGE_SUFFIX="-${IMAGE_DIR}"
+    fi
+    buildImage "${DOCKERFILE_PATH}${IMAGE_DIR}" ${IMAGE_SUFFIX}
+
+    if [[ "dev" == "${IMAGE_TAG}" ]]; then
+        echo -e "\nSkipped pushing dev image"
+    else
+        pushImage ${IMAGE_SUFFIX}
+    fi
+}
+
+for IMAGE_DIR in "${IMAGES[@]}"; do
+    buildPushImage ${IMAGE_DIR}
+done
