c0d60596b5b5c1b179d498f7e8184ac9ae08d2c7
Pete Cornish <outofcoffee@gmail.com>
2017-10-24 18:33:25 +0100

    Adds Slack backend.

--- bots/http-slack/src/main/kotlin/com/gatehill/corebot/bot/BotBootstrap.kt
+++ bots/http-slack/src/main/kotlin/com/gatehill/corebot/bot/BotBootstrap.kt
@@ -1,14 +1,26 @@
 package com.gatehill.corebot.bot
 
 import com.gatehill.corebot.chat.template.FactoryService
+import com.gatehill.corebot.chat.template.TemplateService
+import com.gatehill.corebot.frontend.session.operation.factory.SetRealNameFactory
+import com.gatehill.corebot.frontend.session.operation.factory.SetUsernameFactory
+import com.gatehill.corebot.frontend.session.operation.factory.TerminateSessionFactory
 import com.gatehill.corebot.operation.factory.ShowHelpFactory
 import javax.inject.Inject
 
 /**
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
-class BotBootstrap @Inject constructor(factoryService: FactoryService) {
+class BotBootstrap @Inject constructor(factoryService: FactoryService,
+                                       templateService: TemplateService) {
     init {
+        // core templates
         factoryService.registerFactory(ShowHelpFactory::class.java)
+
+        // ws specific templates
+        templateService.registerClasspathTemplateFile("/session-templates.yml")
+        factoryService.registerFactory(SetUsernameFactory::class.java)
+        factoryService.registerFactory(SetRealNameFactory::class.java)
+        factoryService.registerFactory(TerminateSessionFactory::class.java)
     }
 }
