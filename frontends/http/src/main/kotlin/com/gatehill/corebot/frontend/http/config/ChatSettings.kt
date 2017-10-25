104d78b2f0fabaf063fc16d29cd8166ce4260cc0
Pete Cornish <outofcoffee@gmail.com>
2017-10-25 17:30:54 +0100

    Adds the ability to start with an empty action configuration.

--- frontends/http/src/main/kotlin/com/gatehill/corebot/frontend/http/config/ChatSettings.kt
+++ frontends/http/src/main/kotlin/com/gatehill/corebot/frontend/http/config/ChatSettings.kt
@@ -6,6 +6,6 @@ package com.gatehill.corebot.frontend.http.config
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
 object ChatSettings {
-    val hostname by lazy { System.getenv("HTTP_BIND_HOSTNAME") ?: "localhost" }
+    val hostname by lazy { System.getenv("HTTP_BIND_HOST") ?: "0.0.0.0" }
     val port by lazy { System.getenv("HTTP_BIND_PORT")?.toInt() ?: 8080 }
 }
