ffb3a8a0d3512363d4763250eaee5c4a3c3e3256
Pete Cornish <outofcoffee@gmail.com>
2018-01-26 19:15:18 +0000

    Adds RESTful API store.

---
+++ stores/restapi/src/main/kotlin/com/gatehill/corebot/store/rest/config/StoreSettings.kt
@@ -0,0 +1,32 @@
+package com.gatehill.corebot.store.rest.config
+
+import com.gatehill.corebot.config.EnvironmentSettings
+
+/**
+ * Data store settings.
+ *
+ * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
+ */
+object StoreSettings : EnvironmentSettings() {
+    val baseUrl: String by lazy { getenv("REST_BASE_URL") ?: throw IllegalStateException("Missing REST base URL") }
+
+    val resource: String by lazy { getenv("REST_RESOURCE") ?: throw IllegalStateException("Missing REST resource") }
+
+    /**
+     * Single value that represents the key (essentially the 'primary key') to map to the store's key.
+     */
+    val keyField: String by lazy { getenv("REST_KEY_FIELD") ?: throw IllegalStateException("Missing REST key field") }
+
+    /**
+     * A comma separated list of `key=value` elements that maps field names to body fields.
+     *
+     * Example: `foo=bar,baz=qux`
+     */
+    val valueMap: Map<String, String> by lazy {
+        getenv("REST_VALUE_MAP")
+                ?.split(",")
+                ?.map { it.split("=").let { element -> element[0] to element[1] } }
+                ?.toMap()
+                ?: throw IllegalStateException("Missing REST value map")
+    }
+}
---
+++ stores/restapi/src/test/kotlin/com/gatehill/corebot/store/rest/RestDataStoreImplSpec.kt
@@ -0,0 +1,56 @@
+package com.gatehill.corebot.store.rest
+
+import com.gatehill.corebot.store.partition
+import com.gatehill.corebot.store.rest.config.StoreSettings
+import com.gatehill.corebot.store.rest.util.CrudServer
+import org.amshove.kluent.`should be null`
+import org.amshove.kluent.`should equal`
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+/**
+ * Specification for `RestDataStoreImpl`.
+ */
+object RestDataStoreImplSpec : Spek({
+    val crudServer = CrudServer()
+
+    beforeGroup {
+        crudServer.start()
+    }
+
+    given("a RESTful store") {
+        beforeGroup {
+            StoreSettings.env = mapOf(
+                    "REST_BASE_URL" to "http://localhost:${crudServer.port}/",
+                    "REST_RESOURCE" to "crud",
+                    "REST_KEY_FIELD" to "name",
+                    "REST_VALUE_MAP" to "original=modified"
+            )
+        }
+
+        on("getting a partition") {
+            val store = RestDataStoreImpl()
+            val partition = store.partition<String, Example>("test")
+
+            it("saves objects") {
+                partition["test"] = Example().apply { original = "qux" }
+            }
+            it("loads objects") {
+                partition["test"]!!.original `should equal` "qux"
+            }
+            it("returns null for missing objects") {
+                partition["missing"]!!.original.`should be null`()
+            }
+        }
+    }
+
+    afterGroup {
+        crudServer.stop()
+    }
+})
+
+class Example {
+    var original: String? = null
+}
